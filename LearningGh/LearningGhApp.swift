693e5e41e48a801d96769fee5e4ac4f2481b7653
WChoi <wonhyuk@yonsei.ac.kr>
2023-04-04 23:50:02 +0900

    Initial Commit

---
+++ LearningGh/LearningGhApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGhApp.swift
+//  LearningGh
+//
+//  Created by Wonhyuk Choi on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
